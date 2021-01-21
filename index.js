68f9c30938d144866054468742668062382d8921
ayhand <ayhandani@hotmail.com>
2021-01-21 12:19:00 +0100

    added stream url sharing option thingy

--- index.js
+++ index.js
@@ -1,21 +1,87 @@
+let messageButton;
+let backButton;
+let sendButton;
+let pcLeft;
+let pcLeftMessage;
+let formName;
+let formMessage;
+
+function successCallback() {
+	alert("Siker, köszönjük az üzenetet! :)");
+	backButton.click();
+}
+
+function onSendClick() {
+	let name = (formName.val() || "").trim();
+	let message = (formMessage.val() || "").trim();
+
+	if (!name || !message) {
+		alert("Légyszi, add meg a neved és az üzenetet is!");
+		return;
+	}
+
+	let data = {
+		"entry.129626341": name,
+		"entry.637920671": message,
+	};
+
+	$.ajax({
+		type: 'POST',
+		url: 'https://docs.google.com/forms/d/e/1FAIpQLSc49eQ3VgCLq70yVUlb-iYH9GdzvkiKFRpaasT_Y7Nm00gKoQ/formResponse',
+		data: data,
+		dataType: 'jsonp',
+		complete: () => successCallback()
+	});
+}
+
+function fillEmail() {
+	let mail = "ayhanagoston";
+		mail += String.fromCharCode(63 + 1);
+		mail += "gm";
+		mail += "ail.c";
+		mail += "om";
+		$("#contactlink").attr("href", "mailto:" + mail).text(mail);
+}
+
+async function checkStreamUrl() {
+	let urlParams = new URLSearchParams(window.location.search);
+	let forceEnabled = urlParams.get('fe');
+	let enabled = !!(forceEnabled || await get("enabled"));
+	if(enabled) {
+		let url = await get("url");
+		if(url) {
+			let linkElement = $("#linkElement");
+			linkElement.html(`A közvetítést <a href="${url}">ide kattintva</a> tudod elérni.`);
+		}
+	}
+}
+
+function get(key) {
+	return new Promise((resolve, reject ) => {
+		$.ajax({
+			type: 'GET',
+			url: `https://meghivo-40c80-default-rtdb.europe-west1.firebasedatabase.app/${key}.json`,
+			success: (data) => resolve(data),
+			error: (_, status, error) => {
+				console.error(status, error);
+				reject();
+			}
+		});
+	});
+}
+
 $(() => {
 	setTimeout(() => {
+		// ======================= init elements =======================
+		messageButton = $("#messageButton");
+		backButton = $("#backButton");
+		sendButton = $("#sendButton");
+		pcLeft = $(".pc-left");
+		pcLeftMessage = $(".pc-left-message");
+		formName = $("#form-name");
+		formMessage = $("#form-message");
 
-		function successCallback() {
-			alert("Siker, köszönjük az üzenetet! :)");
-			backButton.click();
-		}
-
-		let messageButton = $("#messageButton");
-		let backButton = $("#backButton");
-		let sendButton = $("#sendButton");
-
-		let pcLeft = $(".pc-left");
-		let pcLeftMessage = $(".pc-left-message");
-
-		let formName = $("#form-name");
-		let formMessage = $("#form-message");
-
+		// ======================= event handlers =======================
 		messageButton.click(() => {
 			pcLeft.addClass("d-none");
 			pcLeftMessage.removeClass("d-none");
@@ -26,34 +92,10 @@ $(() => {
 			pcLeftMessage.addClass("d-none");
 		});
 
-		sendButton.click(() => {
-			let name = (formName.val() || "").trim();
-			let message = (formMessage.val() || "").trim();
+		sendButton.click(() => onSendClick());
 
-			if (!name || !message) {
-				alert("Légyszi, add meg a neved és az üzenetet is!");
-				return;
-			}
-
-			let data = {
-				"entry.129626341": name,
-				"entry.637920671": message,
-			};
-
-			$.ajax({
-				type: 'POST',
-				url: 'https://docs.google.com/forms/d/e/1FAIpQLSc49eQ3VgCLq70yVUlb-iYH9GdzvkiKFRpaasT_Y7Nm00gKoQ/formResponse',
-				data: data,
-				dataType: 'jsonp',
-				complete: () => successCallback()
-			})
-		});
-
-		let mail = "ayhanagoston";
-		mail += String.fromCharCode(63 + 1);
-		mail += "gm";
-		mail += "ail.c";
-		mail += "om";
-		$("#contactlink").attr("href", "mailto:" + mail).text(mail);
+		// ======================= other init logic =======================
+		fillEmail();
+		checkStreamUrl();
 	}, 10);
 });
